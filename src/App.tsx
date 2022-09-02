9ec513fe5b5603786e5d54b00e715ff04df83117
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-02 14:52:12 -0300

    feat: Add eslint configs to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
