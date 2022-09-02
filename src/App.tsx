0bb7a6df5f67715824e25f1c055f8ebe4f9a6a16
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-02 14:32:48 -0300

    feat: Add theme types to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 function App() {
   return (
+    <ThemeProvider theme={defaultTheme}>
     <div className="App">
       <h1>Hello World!</h1>
     </div>
+    </ThemeProvider>
   )
 }
 
