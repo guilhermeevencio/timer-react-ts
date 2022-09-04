48ce24717712caec7d66464d9dba78359e614953
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-04 08:49:30 -0300

    feat: Adds History page infos and styles.

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,68 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+const History = () => {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Duração</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <Status statusColor="green">Concluído</Status>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
+
+export default History
